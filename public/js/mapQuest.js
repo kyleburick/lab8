3e023b5477395c9e504787999b2c748b2f754a67
Kyle Burick <kburick@ucsd.edu>
2021-02-25 22:02:55 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '3IitWbhMRR7KGpn2G7wGYxDKOid14Iq1';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  		center: [32.87871, -117.23582],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+  	});
+
+  	// Add a marker to the map
+  	L.marker([32.88, -117.236]).addTo(map);
 }
